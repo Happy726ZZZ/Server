2cd677ad3b00bbd9aba9b08818753ce5629d9c3a
rabbIT618 <32159686+rabbIT618@users.noreply.github.com>
2019-12-10 19:28:11 +0800

    Update db.go

--- go-server-server/go/db.go
+++ go-server-server/go/db.go
@@ -47,7 +47,7 @@ func createdb() {
 			}
 
 			for n := 0; n < 8; n++ {
-				file_path := "./resource/" + file_name[n] +".md"
+				file_path := "Server/go-server-server/resource//resource/" + file_name[n] +".md"
 				file, _ := os.Open(file_path)
 				defer file.Close()
 				content, _ := ioutil.ReadAll(file)
